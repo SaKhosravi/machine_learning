c94e99fe33809fcb166585b30555502f6e270fed
sadegh khosravi <e03skh@gmail.com>
2022-03-04 19:26:34 +0330

    update perceptron:add train loss and train evaluate metric 1- to calculate loss, you need add loss function name from Loss class as a string in "loss_function" attribute like loss_function="MAE"
    2- to evaluate model you need add evaluate metric function name from Metric class as a string in evaluate_metric attribute like evaluate_metric="accuracy"

--- code/Perceptron/perceptron.py
+++ code/Perceptron/perceptron.py
@@ -1,56 +1,64 @@
 import numpy as np
-from utils.weights import Weights
-from utils.activations import Activations
+from interface import Interface
 from utils.loss import Loss
+from utils.metric import Metric
 
-class MyPerceptron(Activations, Weights,Loss):
-    def __init__(self, iteration, learning_rate):
+
+class MyPerceptron(Interface):
+    def __init__(self, iteration, learning_rate, loss_function, evaluate_metric):
+        # hyper parameter
         self.iteration = iteration
-        self.lr=learning_rate
-        self.weights = None
-        self.bias=0
-        self.loss=list()
+        self.lr = learning_rate
+
+        self.loss = getattr(globals()["Loss"](), loss_function)
+        self.evaluate = getattr(globals()["Metric"](), evaluate_metric)
+
+        self._weights = None
+        self._bias = 0
+
+        self.training_loss = list()
+        self.training_evaluate = list()
 
     def predict(self, x_test):
-        x_test=np.array(x_test)
-        y_preds=[]
+        x_test = np.array(x_test)
+        y_preds = list()
         for x_i in x_test:
-            output=np.dot(x_i.T,self.weights)+self.bias
-            y_pred=self.sign(output)
+            output = np.dot(x_i.T, self._weights) + self._bias
+            y_pred = self.sign(output)
             y_preds.append(y_pred)
 
         return np.array(y_preds)
 
     def fit(self, x_train, y_train):
-        # initial weights
         x_train, y_train = np.array(x_train), np.array(y_train)
-        self.weights = self.getZerosVector(np.shape(x_train)[1]).astype(np.float32)
+
+        # initial weights
+        self._weights = self.getZerosVector(np.shape(x_train)[1]).astype(np.float32)
 
         for iter in range(self.iteration):
             y_preds = []
             for index, x_i in enumerate(x_train):
                 # res = np.dot(self.weights, row)
-                output = np.dot(x_i.T, self.weights) + self.bias
-                y_hat=self.sign(output)
+                output = np.dot(x_i.T, self._weights) + self._bias
+                y_hat = self.sign(output)
                 y_preds.append(y_hat)
                 y = y_train[index]
 
-                #update weights
-                if y - y_hat !=0:
-                    update_rate= self.lr * (y-y_hat)
-                    self.weights += update_rate * x_i
-                    self.bias += update_rate * 1
+                # update weights
+                if y - y_hat != 0:
+                    update_rate = self.lr * (y - y_hat)
+                    self._weights += update_rate * x_i
+                    self._bias += update_rate * 1
 
-            l= self.MAE(y_train,y_preds)
-            self.loss.append(l)
-            # print(l)
-            # print("iteration {} loss:{}".format((iter,l)))
+            # loss and evaluation after each iteration
+            self.training_loss.append(self.loss(y_train, y_preds))
+            self.training_evaluate.append(self.evaluate(y_train, y_preds))
 
-
+        return np.array(self.training_loss), np.array(self.training_evaluate)
 
     def getSign(self, x):
-        output=np.dot(x.T,self.weights)+self.bias
-        if output >  0:
+        output = np.dot(x.T, self._weights) + self._bias
+        if output > 0:
             return 1
         else:
             return 0
--- code/Perceptron/test_preceptron.py
+++ code/Perceptron/test_preceptron.py
@@ -1,31 +1,43 @@
 import numpy as np
-
 from Perceptron.perceptron import MyPerceptron
 from utils.plot_decision_boundaries import PlotDB
 from utils.generate_data_set import GenerateDataSetBlob
 from utils.loss import Loss
-from sklearn.metrics import accuracy_score
-
-gdb = GenerateDataSetBlob()
-X, y = gdb.make_blobs(n_samples=1000, centers=2, n_features=2,
-                      random_state=1,cluster_std=2.8)
-colors = {0: 'red', 1: 'blue'}
-gdb.plot_dataSet(X, y, colors)
-x_train, x_test, y_train, y_test = gdb.train_test_split(X,y,test_size=0.2)
-
 l=Loss()
 
-#initialize
-clf = MyPerceptron(100, learning_rate=0.001)
-#fit
-clf.fit(x_train, y_train)
-#predict
+gdb = GenerateDataSetBlob()
+colors = {0: 'red', 1: 'blue'}
+
+#make blobs
+# X, y = gdb.make_blobs(n_samples=1000, centers=2, n_features=2,
+#                       random_state=4,cluster_std=0.8)
+
+#make mons
+X, y=gdb.make_mons(500,0.15)
+gdb.plot_dataSet(X, y, colors)
+
+x_train, x_test, y_train, y_test = gdb.train_test_split(X,y,test_size=0.2)
+
+# initialize
+clf = MyPerceptron(50, learning_rate=0.001,
+                   loss_function="MAE", evaluate_metric="accuracy")
+
+# step2: fit
+loss, accuracy = clf.fit(x_train, y_train)
+
+# step3: predict
 y_pred=clf.predict(x_test)
-print("test loss", l.MAE(y_test, y_pred))
-print("train loss", clf.loss[len(clf.loss)-1])
-#plot decision boundry
-plt = PlotDB(clf, X, y,"Perceptron")
+print("test accuracy",clf.evaluate(y_pred,y_test))
+
+
+# plot train loss and train accuracy
+l.plot_loss(loss,"perceptron,train loss","iteration","loss")
+l.plot_loss(accuracy,"perceptron,train accuracy","iteration","accuracy")
+
+# plot decision boundary
+name = "perceptron in {} iter".format(50)
+plt = PlotDB(clf, X, y, name)
 plt.plot()
 
-#plot train loss
-l.plot_loss(clf.loss,"Perceptron train loss","iteration","loss")
+
+
