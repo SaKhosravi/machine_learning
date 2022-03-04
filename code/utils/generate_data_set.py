fa1679019d344abaef484b0a9949d7595521c2e5
sadegh khosravi <e03skh@gmail.com>
2022-03-04 19:17:24 +0330

    add make mon data set

--- code/utils/generate_data_set.py
+++ code/utils/generate_data_set.py
@@ -1,20 +1,27 @@
 import numpy as np
 from pandas import DataFrame
-from sklearn.datasets import make_blobs
+from sklearn.datasets import make_blobs, make_moons
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
-class GenerateDataSetBlob():
+
+class GenerateDataSetBlob:
     def __init__(self, ):
         self.X = None
         self.y = None
 
-    def make_blobs(self, n_samples, n_features, centers, random_state,cluster_std):
+    def make_blobs(self, n_samples, n_features, centers, random_state, cluster_std):
         X, y = make_blobs(n_samples=n_samples,
                           n_features=n_features,
                           centers=centers,
                           cluster_std=cluster_std,
                           random_state=random_state)
+        self.X = np.array(X)
+        self.y = np.array(y)
+        return X, y
+
+    def make_mons(self, n_samples, noise):
+        X, y = make_moons(n_samples=n_samples, noise=noise, random_state=0)
         self.X = np.array(X)
         self.y = np.array(y)
         return X, y
@@ -31,11 +38,11 @@ class GenerateDataSetBlob():
         df[target_name] = self.y
         return df
 
-    def plot_dataSet(self,X,y,colors):
+    def plot_dataSet(self, X, y, colors):
         # scatter plot, dots colored by class value
         df = DataFrame(dict(x=X[:, 0], y=X[:, 1], label=y))
         fig, ax = plt.subplots()
         grouped = df.groupby('label')
         for key, group in grouped:
-            group.plot(ax=ax, kind='scatter', x='x', y='y', label=key,color=colors[key])
+            group.plot(ax=ax, kind='scatter', x='x', y='y', label=key, color=colors[key])
         plt.show()
